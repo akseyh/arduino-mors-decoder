4c4baeba0df61ff85766167f5468d15bcbb7c4bf
Şems Yılmaz <yilmazsems@gmail.com>
2021-01-18 09:49:05 +0300

    code fixed

--- code/code.ino
+++ code/code.ino
@@ -2,17 +2,22 @@
 bool isArrayEqual(char [], char []);
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 const int ARRAY_SIZE = 6;
+int NOTA_START = 350;
 int NOTA1 = 440;
 int NOTA2 = 494;
+int NOTA3 = 420;
+int NOTA_ERROR = 600;
 int BUZZER_PIN = 8;
 int BUTTON1_PIN = 6;
 int BUTTON2_PIN = 7;
 char morsCodes[ARRAY_SIZE];
 int codeCounter = 0;
+int letterCounter = 0;
 int HC_ECHO = 9;
 int HC_TRG = 10;
 int MORS_CODE_LENGTH = 26;
-char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char words[] = "                ";
+char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,? ";
 char *letterMorsCodes[] = {
     "._    ", // A
     "_...  ", // B
@@ -39,7 +44,21 @@ char *letterMorsCodes[] = {
     ".__   ", // W
     "_.._  ", // X
     "_.__  ", // Y
-    "__..  " // Z
+    "__..  ", // Z
+    ".____ ", // 1
+    "..___ ", // 2
+    "...__ ", // 3
+    "...._ ", // 4
+    "..... ", // 5
+    "_.... ", // 6
+    "__... ", // 7
+    "___.. ", // 8
+    "____. ", // 9
+    "_____ ", // 0
+    "._._._", // .
+    "__..__", // ,
+    "..__..", // ?
+    "      ", //  
 };
 
 void setup() {
@@ -51,9 +70,13 @@ void setup() {
   pinMode(HC_ECHO, INPUT);
   pinMode(HC_TRG, OUTPUT);
   Serial.begin(9600);
+  tone(BUZZER_PIN, NOTA_START);
+  delay(500);
+  noTone(BUZZER_PIN);
 }
 
 void loop() {
+  bool isEqual = false;
   if(digitalRead(BUTTON1_PIN) == HIGH) {
     morsCodes[codeCounter] = '.';
     codeCounter++;
@@ -81,32 +104,34 @@ void loop() {
   digitalWrite(HC_TRG, LOW);
   duration = pulseIn(HC_ECHO, HIGH);
   if(duration < 600) {
-    lcd.setCursor(0, 0);
-    lcd.print("                ");
-    lcd.setCursor(0, 0);
-    lcd.print(letters[0]);
     for(int i = codeCounter; i<ARRAY_SIZE; i++) {
       morsCodes[i] = ' ';
     }
-    for(int i = 0; i<26; i++) {
-      bool isEqual = isArrayEqual(letterMorsCodes[i], morsCodes);
-      // Serial.println("letterMorsCode: ");
-      // Serial.println(letterMorsCodes[i]);
-      // Serial.println("morsCodes:");
-      // Serial.println(morsCodes);
-      // Serial.println("isEqual");
-      // Serial.println(isEqual);
+    for(int i = 0; i<40; i++) {
+      isEqual = isArrayEqual(letterMorsCodes[i], morsCodes);
       if(isEqual) {
+        words[letterCounter] = letters[i];
         lcd.setCursor(0, 0);
-        lcd.print(letters[i]);
-        Serial.println(letters[i]);
-        for(int i=0; i<ARRAY_SIZE; i++) {
-         morsCodes[i] = ' '; 
-        }
-        codeCounter = 0;
+        lcd.print(words);
+        tone(BUZZER_PIN, NOTA3);
+        delay(200);
+        noTone(BUZZER_PIN);
+        letterCounter++;
+        break;
       }
     }
-    // if(isArrayEqual(chars, morsCodes)) printLcd('A');
+    if(isEqual == false) {
+      tone(BUZZER_PIN, NOTA_ERROR);
+      delay(500);
+      noTone(BUZZER_PIN);
+    }
+    for(int i=0; i<ARRAY_SIZE; i++) {
+      morsCodes[i] = ' '; 
+    }
+    lcd.setCursor(0, 1);
+    lcd.print(morsCodes);
+    codeCounter = 0;
+    delay(500);
   }
 }
 
